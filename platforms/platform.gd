dc97f3e286762dd026f568e7e70e35532a756751
Imma <imma.becker@outlook.de>
2026-01-31 16:18:19 +0100

    platform timeout

--- platforms/platform.gd
+++ platforms/platform.gd
@@ -1,7 +1,7 @@
 class_name Platform
 extends RigidBody3D
 
-@export var lifetime: float = 120.0
+@export var lifetime: float = 45.0
 
 @onready var _timer: Timer = $Timer
 @export var _mass: float = 1.0
@@ -9,4 +9,8 @@ extends RigidBody3D
 func _ready():
     _timer.wait_time = lifetime
     _timer.start()
+    _timer.timeout.connect(_on_Timer_timeout)
     mass = _mass
+
+func _on_Timer_timeout() -> void:
+    queue_free()

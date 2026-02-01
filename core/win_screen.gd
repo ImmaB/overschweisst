0bedc42af5be9342af0413cea24e00a0bf913502
Imma <imma.becker@outlook.de>
2026-02-01 13:17:29 +0100

    platform sounds, retry on weld

--- core/win_screen.gd
+++ core/win_screen.gd
@@ -7,6 +7,10 @@ func _ready() -> void:
     button.pressed.connect(_retry)
     get_tree().paused = true
 
+func _input(event: InputEvent) -> void:
+    if event.is_action_pressed("weld"):
+        _retry()
+
 func _retry() -> void:
     get_tree().paused = false
     get_tree().reload_current_scene()
--- platforms/platform.gd
+++ platforms/platform.gd
@@ -3,7 +3,12 @@ extends RigidBody3D
 
 @export var lifetime: float = 5.0
 @export var _mass: float = 1.0
+@export var base_volume: float = 20.0
+@export var max_volume: float = 70.0
+@export var sound_velocity_factor: float = 2.25
+@export var sound_distance_factor: float = 2.25
 
+@onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var _timer: Timer = $Timer
 
@@ -26,3 +31,8 @@ func _collision(body: Node) -> void:
 	if not _touched_player and body is PlayerCharacter:
 		_touched_player = true
 		remove_forces()
+	if body is Platform:
+		var relative_velocity: Vector3 = to_local(body.linear_velocity) - to_local(linear_velocity)
+		var distance_to_camera: float = global_transform.origin.distance_to(get_viewport().get_camera_3d().global_transform.origin)
+		audio_stream_player_3d.volume_db = clamp(base_volume + relative_velocity.length() * sound_velocity_factor - distance_to_camera * sound_distance_factor, 0.0, max_volume)
+		audio_stream_player_3d.play()

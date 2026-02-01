d050b92fe885eb717b35e21bebbcb3aea6e94b73
Imma <imma.becker@outlook.de>
2026-02-01 10:05:12 +0100

    Step climbing

--- player/player_character.gd
+++ player/player_character.gd
@@ -1,5 +1,5 @@
 class_name PlayerCharacter
-extends CharacterBody3D
+extends StairsCharacter3D
 
 const VISION_CONE := preload("res://player/vision_cone.tscn")
 
@@ -68,7 +68,7 @@ func _physics_process(delta: float) -> void:
             _fall_direction *= -1
         rotation.x = lerp_angle(rotation.x, 0.0, turn_acceleration * delta)
 
-    move_and_slide()
+    move_and_stair_step()
 
 func _calc_movement(move_direction: Vector2, on_floor: bool) -> Vector2:
     if move_direction.length_squared() < 0.01: return Vector2.ZERO
